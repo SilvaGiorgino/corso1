8857e85a61b56497f8ea79a64139b66ac4e42ddd
P13RL <ambrosopierlorenzo@gmail.com>
2021-12-14 14:42:47 +0100

    route

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,28 +6,5 @@
         Titolo di prova
     </x-slot>
 
-    <div>My Page content is here</div>
-
-    {!! Form::open(['url' => 'foo/bar']) !!}
-    {{ Form::radio('category_id', 1) }}
-    {{ Form::text('username'); }}
-    {{  Form::select('size', ['L' => 'Large', 'S' => 'Small']) }}
-
-{!! Form::close() !!}
-
-
-
-
-    @foreach ($blogs as $blog)
-    ----
-        {{ $blog->title }}
-        {!! Form::model($blog, ['url' => ['user.update', $blog->id]]) !!}
-        {{ Form::text('title') }}
-
-        {!! Form::close() !!}
-    -----
-        @endforeach
     <?php $message='ciao'; ?>
-    <x-test type="error" :message="$message" class="mb-4"/>
-    <x-chat/>
 </x-layout>
