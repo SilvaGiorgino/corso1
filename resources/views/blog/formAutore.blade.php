d3921ccb9d143c7ecaf39f9758d87c1049385862
P13RL <ambrosopierlorenzo@gmail.com>
2021-12-14 15:40:35 +0100

    delete navbar

--- resources/views/blog/formAutore.blade.php
+++ resources/views/blog/formAutore.blade.php
@@ -1,9 +1,3 @@
-<div class="flex gap-2 justify-center p-4 text-gray-600 font-medium">
-        <a href="{{ route('libreria.index') }}" class="px-4 py-2 rounded-md bg-white transition-colors hover:bg-gray-100"> Home</a>
-        <a href="{{ route('libreria.formAutore')}}" class="px-4 py-2 rounded-md bg-white transition-colors hover:bg-gray-100">Aggiungi autore</a>
-        <a href="{{ route('libreria.formLibro')}}" class="px-4 py-2 rounded-md bg-white transition-colors hover:bg-gray-100">Aggiungi libro</a>
-    </div>
-
 <x-layout>
     <x-slot name="scripts"></x-slot>
     <x-slot name="title">Inserisci un autore</x-slot>
