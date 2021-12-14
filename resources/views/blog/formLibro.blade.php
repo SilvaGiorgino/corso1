70c7824a6bd10ef2b90a0e5d975d68ff27744a29
Mario <mbot2004@gmail.com>
2021-12-14 15:06:35 +0100

    Generati elementi di date e submit per il form Modificato formLibro

--- resources/views/blog/formLibro.blade.php
+++ resources/views/blog/formLibro.blade.php
@@ -3,12 +3,18 @@
     <x-slot name="title">Inserisci un libro</x-slot>
 
     {{ Form::component('textInput', 'components.text', ['name', 'value'=>null, 'attributes'=>[]]); }}
+    {{ Form::component('dateInput', 'components.date', ['name', 'value'=>null, 'attributes'=>[]]); }}
+    {{ Form::component('submitForm', 'components.submit', ['name', 'value'=>null, 'attributes'=>[]]); }}
+
+
 
     {!! Form::open(['url' => route('libreria.index')]) !!}
     
-    {{ Form::textInput('nome') }}
+    {{ Form::textInput('Nome') }}
+    {{ Form::textInput('Edizione') }}
+    {{ Form::dateInput('Anno Pubblicazione') }}
    
-    {{ Form::submit('Invio'); }}
+    {{ Form::submitForm('Invio'); }}
     
     {!! Form::close() !!}
 </x-layout>
