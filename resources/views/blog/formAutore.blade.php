db1e65d11452a2976162f44040c9a5987af48d51
P13RL <ambrosopierlorenzo@gmail.com>
2021-12-14 15:15:33 +0100

    delete label

--- resources/views/blog/formAutore.blade.php
+++ resources/views/blog/formAutore.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="scripts"></x-slot>
     <x-slot name="title">Inserisce un autore</x-slot>
 
-    {{Form::component('label, components.label', ['name', 'value'=>null]); }}
+    
     {{ Form::component('textInput', 'components.text', ['name', 'value'=>null, 'attributes'=>[]]); }}
     {{ Form::component('dateInput', 'components.date', ['name', 'value'=>null, 'attributes'=>[]]); }}
     {{ Form::component('submitForm', 'components.submit', ['name', 'value'=>null, 'attributes'=>[]]); }}
@@ -10,7 +10,7 @@
 
 
     {!! Form::open(['url' => route('libreria.index')]) !!}
-    {{Form::label('nome', 'Nome autore')}}
+    
     {{ Form::textInput('Nome') }}
     {{ Form::textInput('Cognome') }}
     {{ Form::dateInput('Data di nascita') }}
