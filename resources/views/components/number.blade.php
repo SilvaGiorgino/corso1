81f8eb314d2e275e1d9ccbdb13833c1fb4a7a16e
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2021-12-14 15:47:46 +0100

    Update components style

--- resources/views/components/number.blade.php
+++ resources/views/components/number.blade.php
@@ -1,6 +1,6 @@
 <div class="my-4">
     {{ Form::label($name, $label, ['class' => 'block text-slate-500 text-sm uppercase font-bold mb-2']) }}
-    {{ Form::number($name, $value, array_merge(['class' => 'appearance-none border rounded-mdw-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'], $attributes)) }}
+    {{ Form::number($name, $value, array_merge(['class' => 'appearance-none border rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'], $attributes)) }}
     @error($name)
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
--- resources/views/components/submit.blade.php
+++ resources/views/components/submit.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-8">
-    {{ Form::submit('Invia', array_merge(['class' => 'appearance-none rounded-mdw-full py-4 px-3 bg-blue-800 text-blue-50 font-medium leading-tight focus:outline-none focus:shadow-outline'], $attributes)); }}
+    {{ Form::submit('Invia', array_merge(['class' => 'appearance-none rounded-md w-full py-4 px-3 bg-blue-800 text-blue-50 font-medium leading-tight focus:outline-none focus:shadow-outline'], $attributes)); }}
     @error($name)
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
