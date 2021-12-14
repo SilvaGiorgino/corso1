b7739000b5288f5511ffdf7c7ec113a37da98589
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2021-12-14 15:12:41 +0100

    Upgrade tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,12 +15,13 @@
         <a href="{{ route('libreria.index') }}" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100"> Home</a>
         <a href="{{ route('libreria.formAutore')}}" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100">Aggiungi autore</a>
         <a href="{{ route('libreria.formLibro')}}" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100">Aggiugni libro</a>
-
     </div>
     <div class="container mx-auto p-4">
-        <h1 class="mt-8 mb-4 font-bold text-blue-800 text-4xl">Pagina di benvenuto</h1>
-        <p class="leading-relaxed text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        
+        <div class="bg-sky-200">
+            <h1 class="mt-8 mb-4 font-bold text-blue-800 text-4xl">Pagina di benvenuto</h1>
+            <p class="leading-relaxed text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+
         <!-- Elenco libri -->
         <h3 class="mt-8 font-bold text-gray-400 uppercase">Elenco dei libri</h3>
         <div class="my-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
