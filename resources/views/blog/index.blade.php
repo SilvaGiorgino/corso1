e855c2f44c8a23dddc4ea059a1381d3ed676b98c
P13RL <ambrosopierlorenzo@gmail.com>
2021-12-14 14:47:41 +0100

    route

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,5 +6,26 @@
         Titolo di prova
     </x-slot>
 
+    <div>My Page content is here</div>
+
+    {!! Form::open(['url' => 'foo/bar']) !!}
+    {{ Form::radio('category_id', 1) }}
+    {{ Form::text('username'); }}
+    {{  Form::select('size', ['L' => 'Large', 'S' => 'Small']) }}
+
+{!! Form::close() !!}
+
+
+
+
+    @foreach ($blogs as $blog)
+    ----
+        {{ $blog->title }}
+        {!! Form::model($blog, ['url' => ['user.update', $blog->id]]) !!}
+        {{ Form::text('title') }}
+
+        {!! Form::close() !!}
+    -----
+        @endforeach
     <?php $message='ciao'; ?>
 </x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 <body>
     <!-- Menu -->
     <div class="flex gap-2 justify-center p-4">
-        <a href="#" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100">Link 1</a>
+        <a href="#" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100"> {{ libreria.index }}</a>
         <a href="#" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100">Link 2</a>
     </div>
     <div class="container mx-auto">
