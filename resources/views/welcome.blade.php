f4903b5360a3b29e14b482b9de6ff66d1da8128c
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2021-12-14 14:29:37 +0100

    Update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,17 @@
     <title>Welcome page</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-    <body>
-        <div class="m-2">
-            <p class="text-4xl">Pagina benvenuto</p>
-        </div>
-        <div class="m-2">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-    </body>
+
+<body>
+    <!-- Menu -->
+    <div class="flex gap-2 justify-center p-4">
+        <a href="#" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100">Link 1</a>
+        <a href="#" class="px-4 py-2 rounded-lg bg-white transition-colors hover:bg-gray-100">Link 2</a>
+    </div>
+    <div class="container mx-auto">
+        <h1 class="mt-8 mb-4 font-bold text-blue-800 text-4xl">Pagina di benvenuto</h1>
+        <p class="leading-relaxed text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</body>
+
 </html>
