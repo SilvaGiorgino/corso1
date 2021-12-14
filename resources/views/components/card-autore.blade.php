6a214ac5b4b993ee7a156cc7e4573e781bd32ba0
dddddavid02 <73659254+dddddavid02@users.noreply.github.com>
2021-12-14 14:40:14 +0100

    Update author card

--- resources/views/components/card-autore.blade.php
+++ resources/views/components/card-autore.blade.php
@@ -1,16 +1,14 @@
-<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 m-8">
-    <div class="flex flex-col gap-y-4 rounded-md border border-gray-200 transition-shadow hover:shadow-sm p-6">
-        <div class="flex justify-between items-center">
-            <h3 class="text-2xl font-medium">Nome Cognome</h3>
-            <!--<span class="bg-gray-100 px-3 py-1 rounded-md text-sm font-medium text-gray-600">2021</span>-->
-        </div>
-        <div class="flex gap-2 flex-wrap">
-            <span class="bg-blue-200 text-sm px-3 py-1 rounded-full">Nazionalità</span>
-            <span class="border text-sm px-3 py-1 rounded-full">Data di nascita</span>
-        </div>
-        <p>
-            <!--Note-->
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget accumsan justo, eu rutrum quam. In molestie ligula at ultricies luctus. In auctor ipsum ante, a elementum ligula commodo in.
-        </p>
+<div class="flex flex-col gap-y-4 rounded-md border border-gray-200 transition-shadow hover:shadow-sm p-6">
+    <div class="flex justify-between items-center">
+        <h3 class="text-2xl font-medium">Nome Cognome</h3>
+        <!--<span class="bg-gray-100 px-3 py-1 rounded-md text-sm font-medium text-gray-600">2021</span>-->
     </div>
-</div> 
+    <div class="flex gap-2 flex-wrap">
+        <span class="bg-blue-200 text-sm px-3 py-1 rounded-full">Nazionalità</span>
+        <span class="border text-sm px-3 py-1 rounded-full">Data di nascita</span>
+    </div>
+    <p>
+        <!--Note-->
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget accumsan justo, eu rutrum quam. In molestie ligula at ultricies luctus. In auctor ipsum ante, a elementum ligula commodo in.
+    </p>
+</div>
