f4bfe93719489415d6c05d70a7fb8e2cd74f9ac7
Lucasambuu <93621294+Lucasambuu@users.noreply.github.com>
2021-12-14 14:48:32 +0100

    correzione

--- resources/views/blog/formAutore.blade.php
+++ resources/views/blog/formAutore.blade.php
@@ -5,10 +5,10 @@
     <label for="cognome">Cognome:</label>
     <input type="text" id="cognome" name="cognome" placeholder="Inerire il cognome">
 
-    <label for="nazionalità">
+    <label for="nazionalità">Nazionalità:</label>
     <input type="text" id="nazionalita" name="nazionalita" placeholder="Inserire la nazionalità">
 
-    <label for="dataN">
+    <label for="dataN">Data di nascita:</label>
     <input type="date" id="dataN" name="dataN" name="dataN">
 
     <input class='button small expanded' type="submit" name="submit" value="Aggiungi">
